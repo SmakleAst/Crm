549f64d12dca8dd8b175900eae05a7da04b9cf0c
SmakleAst <SmakleLover@yandex.ru>
2024-09-05 14:43:34 +0300

    Added db context and entity configuration

---
+++ Crm.Backend/Identity.Application/Interfaces/IIdentityDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Identity.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Application.Interfaces
+{
+    public interface IIdentityDbContext
+    {
+        DbSet<User> Users { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
