2dc55d05dc713703b8090e3ec52764e478afe083
SmakleAst <SmakleLover@yandex.ru>
2024-09-06 15:41:46 +0300

    Set up swagger options

---
+++ Crm.Backend/Identity.Api/ConfigureSwaggerOptions.cs
@@ -0,0 +1,74 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+
+namespace Identity.Api
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) =>
+            _provider = provider;
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in _provider.ApiVersionDescriptions)
+            {
+                var apiVersion = description.ApiVersion.ToString();
+                options.SwaggerDoc(description.GroupName,
+                    new OpenApiInfo
+                    {
+                        Version = apiVersion,
+                        Title = $"Crm Api {apiVersion}",
+                        Description = "Web api for working with Crm.",
+                        //TermsOfService = new Uri(""),
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Denis Bovtunov (developer)",
+                            Email = "den.bovtunov@yandex.ru",
+                            Url = new Uri("https://t.me/Smakle_ast")
+                        },
+                        License = new OpenApiLicense
+                        {
+                            Name = "©",
+                            //Url = new Uri("")
+                        }
+                    });
+
+                options.AddSecurityDefinition($"AuthToken {apiVersion}",
+                    new OpenApiSecurityScheme
+                    {
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.Http,
+                        BearerFormat = "JWT",
+                        Scheme = "bearer",
+                        Name = "Authorization",
+                        Description = "Authorization token",
+                    });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = $"AuthToken {apiVersion}",
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+
+                options.CustomOperationIds(apiDescription =>
+                    apiDescription.TryGetMethodInfo(out MethodInfo methodInfo)
+                        ? methodInfo.Name
+                        : null);
+            }
+        }
+    }
+}
