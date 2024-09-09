abdd280d67925aa4379caa7d3b3c72956a649707
SmakleAst <SmakleLover@yandex.ru>
2024-09-09 17:22:36 +0300

    Added domain models

---
+++ Crm.Backend/Crm.Domain/Entities/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace Crm.Domain.Entities
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? EditDate { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
