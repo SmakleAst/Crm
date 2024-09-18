7bf7df7f9921488b6878660889e31da0ac0ad278
SmakleAst <SmakleLover@yandex.ru>
2024-09-18 17:57:05 +0300

    Add mapping configuration

---
+++ Crm.Backend/Crm.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace Crm.Application.Common.Mappings
+{
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingConfiguration(assembly);
+
+        private void ApplyMappingConfiguration(Assembly assembly)
+        {
+            var types = assembly.GetTypes()
+                .Where(type => type.GetInterfaces()
+                    .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
