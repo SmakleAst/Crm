1d6f01c5cc3e5f613d2037353ae0525a3dfcba12
SmakleAst <SmakleLover@yandex.ru>
2024-09-18 19:52:38 +0300

    Create client command implementation

---
+++ Crm.Backend/Crm.Application/Clients/Commands/CreateClient/CreateClientCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Crm.Application.Clients.Commands.CreateClient
+{
+    public class CreateClientCommand : IRequest<Guid>
+    {
+        public string ClientCode { get; set; }
+        public string? LastName { get; set; }
+        public string? Name { get; set; }
+        public string? MiddleName { get; set; }
+        public DateTime? BirthDay { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? PostalCode { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+    }
+}
---
+++ Crm.Backend/Crm.Application/Clients/Commands/CreateClient/CreateClientCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using Crm.Application.Common.Exceptions;
+using Crm.Application.Interfaces;
+using Crm.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crm.Application.Clients.Commands.CreateClient
+{
+    public class CreateClientCommandHandler : IRequestHandler<CreateClientCommand, Guid>
+    {
+        private readonly ICrmDbContext _dbContext;
+
+        public CreateClientCommandHandler(ICrmDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Guid> Handle(CreateClientCommand request, CancellationToken cancellationToken)
+        {
+            var client = await _dbContext.Clients
+                .FirstOrDefaultAsync(client => client.ClientCode.ToLower().Equals(request.ClientCode.ToLower()));
+
+            if (client != null)
+            {
+                throw new AlreadyExistsException(nameof(Client), request.ClientCode);
+            }
+
+            client = new Client
+            {
+                Id = Guid.NewGuid(),
+                ClientCode = request.ClientCode,
+                LastName = request.LastName,
+                Name = request.Name,
+                MiddleName = request.MiddleName,
+                BirthDay = request.BirthDay,
+                Email = request.Email,
+                Phone = request.Phone,
+                PostalCode = request.PostalCode,
+                City = request.City,
+                Country = request.Country,
+                CreationDate = DateTime.Now,
+            };
+
+            await _dbContext.Clients.AddAsync(client, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return client.Id;
+        }
+    }
+}
