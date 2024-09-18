59eaec36435ffbb9ea39701b5b2119c3dea6fff5
SmakleAst <SmakleLover@yandex.ru>
2024-09-18 17:39:12 +0300

    DbContext implementation

---
+++ Crm.Backend/Crm.Application/Interfaces/ICrmDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Crm.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crm.Application.Interfaces
+{
+    public interface ICrmDbContext
+    {
+        DbSet<Client> Clients { get; set; }
+        DbSet<Funnel> Funnels { get; set; }
+        DbSet<Deal> Deals { get; set; }
+        DbSet<Note> Notes { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
